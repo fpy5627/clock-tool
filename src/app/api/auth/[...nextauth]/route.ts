ddc2ad6016d0b9ade6665436e7e7352f753d4d2a
fpy5627 <t370@qq.com>
2025-11-11 23:58:45 +0800

    优化SEO

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,14 +2,14 @@ import { handlers } from "@/auth";
 
 const { GET: originalGET, POST: originalPOST } = handlers;
 
-export const GET = async (req: Request, context: any) => {
-  const response = await originalGET(req, context);
+export const GET = async (req: Request) => {
+  const response = await originalGET(req);
   response.headers.set("X-Robots-Tag", "noindex, nofollow");
   return response;
 };
 
-export const POST = async (req: Request, context: any) => {
-  const response = await originalPOST(req, context);
+export const POST = async (req: Request) => {
+  const response = await originalPOST(req);
   response.headers.set("X-Robots-Tag", "noindex, nofollow");
   return response;
 };
